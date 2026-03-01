fa2143346960be12fc4c2d37f2663126f3aec89f
GriffScripts <grant.granny05@gmail.com>
2026-03-01 22:46:18 +0000

    Add files via upload

---
+++ shared.lua
@@ -0,0 +1,126 @@
+local function DetectAndSetFramework()
+    if Link.framework ~= 'auto' then
+        return
+    end
+    
+    local frameworks = {
+        ['es_extended'] = 'esx',
+        ['qb-core'] = 'qbcore',
+        ['qbx-core'] = 'qbox',
+        ['ox_core'] = 'ox',
+        ['vRP-framework'] = 'vrp',
+    }
+    
+    for resource, config in pairs(frameworks) do
+        if GetResourceState(resource) == 'started' then
+            Link.framework = config
+            Debug('set framework', config)
+            return
+        end
+    end
+    
+    print('^1Framework not detected. If you are using a framework. Please configure it in the config.lua of ^6kq_link')
+    Link.framework = 'none'
+end
+
+local function DetectAndSetInventory()
+    if Link.inventory ~= 'auto' then
+        return
+    end
+    
+    Link.inventory = 'framework'
+    
+    local inventories = {
+        ['ox_inventory'] = 'ox_inventory',
+        ['qs-inventory'] = 'qs-inventory',
+        ['ps-inventory'] = 'ps-inventory',
+        ['ak47_inventory'] = 'ak47_inventory',
+        ['core_inventory'] = 'core_inventory',
+        ['minventory'] = 'codem-inventory',
+        ['inventory'] = 'chezza',
+    }
+    
+    for resource, config in pairs(inventories) do
+        if GetResourceState(resource) == 'started' then
+            Link.inventory = config
+            Debug('set inventory', config)
+            return
+        end
+    end
+end
+
+local function DetectAndSetVehicleKeys()
+    if Link.vehiclekeys ~= 'auto' then
+        return
+    end
+
+    local vehiclekeys = {
+        ['qbx_vehiclekeys'] = 'qbx_vehiclekeys',
+        ['qb-vehiclekeys'] = 'qb-vehiclekeys',
+        ['wasabi_carlock'] = 'wasabi_carlock',
+        ['vehicles_keys'] = 'jaksam',
+        ['MrNewbVehicleKeys'] = 'mrnewb',
+        ['Renewed-Vehiclekeys'] = 'renewed',
+    }
+
+    for resource, config in pairs(vehiclekeys) do
+        if GetResourceState(resource) == 'started' then
+            Link.vehiclekeys = config
+            Debug('set vehiclekeys', config)
+            return
+        end
+    end
+
+    Link.vehiclekeys = 'standalone'
+end
+
+local function DetectAndSetNotifications()
+    if Link.notifications ~= 'auto' then
+        return
+    end
+
+    -- Prioritize dedicated notification resources
+    if GetResourceState('gs-notify') == 'started' then
+        Link.notifications = 'gs-notify'
+        Debug('set notifications', 'gs-notify')
+        return
+    end
+
+    if GetResourceState('okokNotify') == 'started' then
+        Link.notifications = 'okokNotify'
+        Debug('set notifications', 'okokNotify')
+        return
+    end
+
+    if GetResourceState('codem-notification') == 'started' then
+        Link.notifications = 'codem-notification'
+        Debug('set notifications', 'codem-notification')
+        return
+    end
+
+    if GetResourceState('mythic_notify') == 'started' then
+        Link.notifications = 'mythic'
+        Debug('set notifications', 'mythic')
+        return
+    end
+
+    if GetResourceState('17mov_Hud') == 'started' then
+        Link.notifications = '17mov'
+        Debug('set notifications', '17mov')
+        return
+    end
+
+    -- Fallback to libraries/frameworks
+    if GetResourceState('ox_lib') == 'started' then
+        Link.notifications = 'ox'
+        Debug('set notifications', 'ox')
+        return
+    end
+
+    Link.notifications = 'standalone'
+end
+
+DetectAndSetFramework()
+DetectAndSetInventory()
+DetectAndSetVehicleKeys()
+DetectAndSetNotifications()
